4cc2ec2bd54ad92a38539574266254b23fd5816b
Igor Shabat <shabat.igor@gmail.com>
2017-09-16 15:35:13 +0300

    #5 Connected SearchHeader to redux store and implemented redux store

---
+++ src/ducks/search.js
@@ -0,0 +1,82 @@
+const initialState = {
+  type: '',
+  query: '',
+  types: [],
+  fetching: false,
+  error: '',
+};
+
+const FETCH_SWAPI_TYPES_STARTED = 'swapi/search/FETCH_SWAPI_TYPES_STARTED';
+function fetchSwapiTypesStarted(payload = { fetching: true }) {
+  return {
+    payload,
+    type: FETCH_SWAPI_TYPES_STARTED,
+  };
+}
+
+const FETCHED_SWAPI_TYPES_SUCCESS = 'swapi/search/FETCHED_SWAPI_TYPES_SUCCESS';
+function fetchedSwapiTypesSuccess(payload) {
+  return {
+    payload: Object.assign({}, payload, { fetching: false }),
+    type: FETCHED_SWAPI_TYPES_SUCCESS,
+  };
+}
+
+const FETCHED_SWAPI_TYPES_FAILED = 'swapi/search/FETCHED_SWAPI_TYPES_FAILED';
+function fetchedSwapiTypesFailed(payload) {
+  return {
+    payload: Object.assign({}, payload, { fetching: false }),
+    type: FETCHED_SWAPI_TYPES_FAILED,
+  };
+}
+
+export function fetchSwapiTypes(payload) {
+  return (dispatch) => {
+    dispatch(fetchSwapiTypesStarted(payload));
+
+    return window
+      .fetch('https://swapi.co/api/')
+      .then(res => res.json())
+      .then((json) => {
+        const types = Object.keys(json);
+
+        dispatch(fetchedSwapiTypesSuccess({ types }));
+      })
+      .catch(error => dispatch(fetchedSwapiTypesFailed({ error: error.message })));
+  };
+}
+
+const UPDATE_SEARCH_TYPE = 'swapi/search/UPDATE_SEARCH_TYPE';
+export function updateSearchType(payload) {
+  return {
+    payload,
+    type: UPDATE_SEARCH_TYPE,
+  };
+}
+
+const UPDATE_SEARCH_QUERY = 'swapi/search/UPDATE_SEARCH_QUERY';
+export function updateSearchQuery(payload) {
+  return {
+    payload,
+    type: UPDATE_SEARCH_QUERY,
+  };
+}
+
+export default function reducer(state = initialState, { type, payload }) {
+  console.log(type);
+
+  switch (type) {
+    case FETCH_SWAPI_TYPES_STARTED:
+      return Object.assign({}, state, payload);
+    case FETCHED_SWAPI_TYPES_SUCCESS:
+      return Object.assign({}, state, payload);
+    case FETCHED_SWAPI_TYPES_FAILED:
+      return Object.assign({}, state, payload);
+    case UPDATE_SEARCH_TYPE:
+      return Object.assign({}, state, payload);
+    case UPDATE_SEARCH_QUERY:
+      return Object.assign({}, state, payload);
+    default:
+      return state;
+  }
+}
